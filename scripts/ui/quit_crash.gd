9858aed4cbdbce992111c4f68e63bab07946889f
TILTED.TV <tiltdevofficial@gmail.com>
2026-04-24 19:33:17 +1200

    'Accidental' Crash when Quitting

--- scripts/ui/quit_crash.gd
+++ scripts/ui/quit_crash.gd
@@ -1,9 +1,26 @@
 @tool
 extends Button
 
+const nulltheme: Theme = preload("res://assets/themes/null.tres")
+
 func _pressed() -> void:
 	show_error("Button 'MainMenu.VBoxContainer.HBoxContainer.Quit' cannot find script 'scripts/ui/quit.gd'")
 
 func show_error(message: String) -> void:
-	OS.alert(message, "Fatal Error")
+	var dialog := AcceptDialog.new()
+	add_child(dialog)
+	dialog.title = "Fatal Error"
+	dialog.dialog_text = message
+	dialog.force_native = true
+	dialog.theme = nulltheme
+	dialog.popup_centered()
+	
+	dialog.confirmed.connect(func ():
+		dialog.queue_free()
+		quit()
+	)
+	
+	dialog.canceled.connect(dialog.queue_free)
+
+func quit() -> void:
 	get_tree().quit()
